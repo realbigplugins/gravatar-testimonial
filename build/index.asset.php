b38a952e0c8fa9ec02c2f8a45f9d6d4ae1e17e8a
Joel Yoder <github@joelyoder.com>
2021-09-15 14:47:24 -0400

    Rebuilt

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'cffcaaa36ad52436c66960a9ae51e172');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '01308c0424c2aadc632bde5a8f882138');
